ae23e04fb574b6dad4ded947c307d26615561de5
Aaron <aaron@aarondavis.co.uk>
2016-12-23 15:06:55 +0000

    Removed unused variables
    removed unused variables
    created catchable snowflake constructor

--- Snow/sketch.js
+++ Snow/sketch.js
@@ -58,11 +58,41 @@ function snowFlake(){
   this.x = random(-width/4, width);
   this.y = random(0, height);
   this.ySpeed = random(5, 20);
-  this.visible = true;
+
+  this.show = function(){
+    fill(255,255,255);
+    ellipse(this.x, this.y, map(this.ySpeed, 20, 5, 9, 1));
+  }
+  
+  this.update = function(){
+    this.y = this.y + this.ySpeed;
+    this.x = this.x + wind.value();
+    
+    if(this.y > height){
+      this.y = 0;
+      this.ySpeed = random(5, 20);
+      this.x = random(0,width);
+      snowFlakesFallenOnGround++;
+    }
+
+    if(this.x < 0){
+      this. x = width;
+    }
+    if(this.x > width){
+      this.x = 0;
+    }
+  }
+}
+
+
+function catchFlake(){
+  this.x = random(-width/4, width);
+  this.y = random(0, height);
+  this.ySpeed = random(3,9);
   
   this.show = function(){
     fill(255,255,255);
-    ellipse(this.x, this.y, map(this.ySpeed, 20, 5, 25, 3));
+    ellipse(this.x, this.y, map(this.ySpeed, 9, 3, 20, 12));
   }
   
   this.update = function(){
